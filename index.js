82baf46d1d7562442eef0097573316a2cf7e21e6
Roberto Busó-García <robertobuso@gmail.com>
2018-07-02 18:46:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// add solution here
+function theBeatlesPlay(musician, instrument) {
+  var newArray = [];
+
+  for (let i = 0; i < musician.length;  i++) {
+    newArray.push(`${musician[i]} plays ${instrument[i]`);
+}
+return newArray;
+}
+
+function johnLennonFacts(facts) {
+
+var i = 0;
+
+while (i < facts.length) {
+
+facts[i] = `${facts[i]}!!!`;
+
+}
+
+return facts;
+
+}
+
+function iLoveTheBeatles(number) {
+
+var newArray = [];
+
+var i = 0;
+
+do {
+
+i++;
+
+newArray.push(“I love the Beatles!”);
+
+} while (i < 15);
+return newArray;
+}
+}
+}
